29e37358b03b6051f3621f72a1467db33d26888d
Samrudh M gowda <samrudhmgowda61@gmail.com>
2025-03-11 19:10:23 +0530

    Feat

---
+++ thankyou/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+  title: 'Thank You | West-Mount',
+};
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export default function ThankyouLayout({ children }: Readonly<LayoutProps>) {
+  return (
+    <>
+    
+      {children}
+
+    </>
+  );
+}
